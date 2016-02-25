41278f25df38a0d6a61c1e8bc59f44d08d5cc630
yangxg <yangxuguangvip@163.com>
2016-02-25 17:17:12 +0800

    update

--- logic/service/main.lua
+++ logic/service/main.lua
@@ -11,9 +11,9 @@ end
 server.start(function()
 	server.register(".mainservice")
 	--分布式服务
-	server.newservice("snlua distribute")
+	--server.newservice("snlua distribute")
 	--网关服务
-	server.newservice("snlua logicsocket")
+	--server.newservice("snlua logicsocket")
 
 	--server.timeout(300, test)
 end)
